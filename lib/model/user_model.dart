885cb90c4be82838500d5ac5938e712ef5156960
meetjanani <meetjanani47@gmail.com>
2023-07-24 22:58:19 +0530

    Added User Model class as a Request data class

---
+++ lib/model/user_model.dart
@@ -0,0 +1,16 @@
+
+import 'package:json_annotation/json_annotation.dart';
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+  String name = "";
+  String phoneNumber = "";
+  String emailAddress = "";
+  String createAt = DateTime.now().toString();
+
+  UserModel({required this.name,required this.phoneNumber,required this.emailAddress,});
+  factory UserModel.fromJson(Map<String,dynamic> data) => _$UserModelFromJson(data);
+
+  Map<String,dynamic> toJson() => _$UserModelToJson(this);
+}
