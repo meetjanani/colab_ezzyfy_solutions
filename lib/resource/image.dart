cc5ee42f474316d4e8634def921034b4d9d2d001
akshayfichadia <akshay@loyyo.nl>
2023-07-26 22:45:45 +0530

    create project

--- lib/resource/image.dart
+++ lib/resource/image.dart
@@ -1,7 +1,7 @@
 
 
 const bg = 'assets/Background.png';
-
+const upload = 'assets/up.png';
 
 
 
--- lib/ui/pages/home_dashboard/create_project_page.dart
+++ lib/ui/pages/home_dashboard/create_project_page.dart
@@ -2,6 +2,7 @@ import 'package:colab_ezzyfy_solutions/controller/create_project_controller.dart
 import 'package:colab_ezzyfy_solutions/resource/extensions.dart';
 import 'package:colab_ezzyfy_solutions/resource/image.dart';
 import 'package:colab_ezzyfy_solutions/ui/widget/all_widget.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -37,6 +38,16 @@ class CreateProjectPage extends GetView<CreateProjectController> {
                     SizedBox(
                       height: 20,
                     ),
+                    Row(
+                      children: [
+                        SizedBox(width: 20,),
+                        CircleAvatar(),
+                        SizedBox(width: 10,),
+                        text('Hello,', Colors.white, 14, FontWeight.normal),
+                        SizedBox(width: 5,),
+                        text('Ronaldo', Colors.white, 14, FontWeight.bold),
+                      ],
+                    )
 
                   ],
                 ),
@@ -52,7 +63,25 @@ class CreateProjectPage extends GetView<CreateProjectController> {
                   hintText: 'Enter Project name'
               ),
               ),
-
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20,0,20,0),
+                child: DottedBorder(
+                  color: Colors.grey.shade700,
+                  borderType: BorderType.RRect,
+                  dashPattern: [6,5],
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: [
+                        Image.asset(upload),
+                        SizedBox(height: 50,),
+                        text('Upload Media', Colors.blue, 14, FontWeight.normal)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
               SizedBox(height: 10,),
               Padding(padding: const EdgeInsets.fromLTRB(20,0,20,20),
                 child:  inputField(
